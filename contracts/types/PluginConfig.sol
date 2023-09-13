b518950e68c5092dfec7edd4ee5e9dc3fdfd4b46
Vladislav Volosnikov <Vladyan18@gmail.com>
2023-09-13 15:23:27 +0300

    ADd dummy plugin

--- contracts/types/PluginConfig.sol
+++ contracts/types/PluginConfig.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.4;
 
-import "@cryptoalgebra/core/contracts/libraries/Plugins.sol";
+import {Plugins} from "@cryptoalgebra/core/contracts/libraries/Plugins.sol";
 
 type PluginConfig is uint8;
 
